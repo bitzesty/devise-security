fba2dd6dea5571bd979284eb7f7bbd67556eb8b0
Ruslan Khamidullin <rusllonrails@gmail.com>
2019-05-15 22:22:49 +0300

    [SESSION LIMITABLE MODULE] added ability to skip
    Based on PR https://github.com/devise-security/devise-security/pull/79
    Made by Mister https://github.com/kirichkov

--- lib/devise-security/hooks/session_limitable.rb
+++ lib/devise-security/hooks/session_limitable.rb
@@ -4,11 +4,22 @@
 # This is only triggered when the user is explicitly set (with set_user)
 # and on authentication. Retrieving the user from session (:fetch) does
 # not trigger it.
+
+# With patch from https://github.com/devise-security/devise-security/pull/79
+# made by Mister https://github.com/kirichkov
+
 Warden::Manager.after_set_user except: :fetch do |record, warden, options|
-  if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope])
-    unique_session_id = Devise.friendly_token
-    warden.session(options[:scope])['unique_session_id'] = unique_session_id
-    record.update_unique_session_id!(unique_session_id)
+  scope = options[:scope]
+  skip  = options[:skip_session_limitable]
+
+  if record.respond_to?(:update_unique_session_id!) && warden.authenticated?(options[:scope]) 
+    if !skip
+      unique_session_id = Devise.friendly_token
+      warden.session(scope)['unique_session_id'] = unique_session_id
+      record.update_unique_session_id!(unique_session_id)
+    else
+      warden.session(scope)['devise.skip_session_limitable'] = true
+    end
   end
 end
 
@@ -17,10 +28,10 @@ end
 # If so, the old account is logged out and redirected to the sign in page on the next request.
 Warden::Manager.after_set_user only: :fetch do |record, warden, options|
   scope = options[:scope]
-  env   = warden.request.env
+  skip  = warden.session(options[:scope])['devise.skip_session_limitable']
 
   if record.respond_to?(:unique_session_id) && warden.authenticated?(scope) && options[:store] != false
-    if record.unique_session_id != warden.session(scope)['unique_session_id'] && !env['devise.skip_session_limitable']
+    if record.unique_session_id != warden.session(scope)['unique_session_id'] && !skip
       warden.raw_session.clear
       warden.logout(scope)
       throw :warden, scope: scope, message: :session_limited
